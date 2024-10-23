3914e3a58e938b83a2ee7de37bd46342b10ce49c
ahmedtarek52 <a7mdtarek52@gmail.com>
2024-10-23 14:57:48 +0300

    final commit on function

--- TaskGP.cpp
+++ TaskGP.cpp
@@ -1,34 +1,130 @@
 #include <iostream>
 using namespace std;
 
-int sum(int x, int y){
-    return x+y;
-}
-
-int subtact(int x, int y){
-    return x-y;
-}
-
-int multiplication(int x, int y){
-    return x*y;
-}
-
-int division(int x , int y)
+int sum(int arr[], int size)
 {
-    return x/y;
+    int result = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        result += arr[i];
+    }
+    return result;
 }
 
-int power(int x)
+int subtract(int arr[], int size)
 {
-    return x*x;
+    int result = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        result -= arr[i];
+    }
+    return result;
+}
+
+int multiply(int arr[], int size)
+{
+    int result = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        result *= arr[i];
+    }
+    return result;
+}
+
+int division(int x, int y)
+{
+    if (y == 0)
+    {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+    }
+    return x / y;
+}
+
+int sqrPower(int x)
+{
+    return x * x;
 }
 
 int modulas(int x, int y)
 {
- return x%y; 
+    if (y == 0)
+    {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+    };
+    return x % y;
 }
 
 int main()
 {
+    int size;
+    int num;
+
+    cout << "please select operation you want to do (+ , - , / , % , ^)" << '\n';
+    char operation;
+    cin >> operation;
+    switch (operation)
+    {
+    case '/': {
+        cout << "please enter your numbers" << '\n';
+        int fnum, lnum;
+        cin >> fnum;
+        cin >> lnum;
+        cout << "the final result is: " << division(fnum, lnum) << '\n';
+        break;
+    }
+    case '%': {
+        cout << "please enter your numbers" << '\n';
+        int fnum, lnum;
+        cin >> fnum;
+        cin >> lnum;
+        cout << "the final result is: " << modulas(fnum, lnum) << '\n';
+        break;
+    }
+    case '^': {
+        cout << "please enter your numbers to get the square" << '\n';
+        cin >> num;
+        cout << "the final result is: " << sqrPower(num) << '\n';
+        break;
+    }
+    case '+': {
+        cout << "how many number you want to sum" << '\n';
+        cin >> size;
+        int* arr = new int[size];
+
+        for (int i =0; i < size; i++) {
+            cin >> arr[i];
+        }
+        cout << "the final result is: " << sum(arr,size) << '\n';
+        break;
+    }
+    case '-': {
+        cout << "how many number you want to Subtract" << '\n';
+        cin >> size;
+        int* arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+        }
+        cout << "the final result is: " << subtract(arr, size) << '\n';
+        break;
+    }
+    case '*': {
+        cout << "how many number you want to Multiply" << '\n';
+        cin >> size;
+        int* arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+        }
+        cout << "the final result is: " << multiply(arr, size) << '\n';
+        break;
+    }
+
+    default:
+        cout << "invalid input! please enter one of the operations";
+        break;
+    }
 
 }
