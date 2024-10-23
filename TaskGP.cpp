80a5cb96c430ec21c3e99f4e65412ffa62581164
Hassan El Sayed <55570333+hassan-prog@users.noreply.github.com>
2024-10-23 14:14:46 +0300

    added Can't divide by zero

--- TaskGP.cpp
+++ TaskGP.cpp
@@ -1,46 +1,67 @@
 #include <iostream>
 using namespace std;
 
-int sum(int arr[], int size) {
+int sum(int arr[], int size)
+{
     int result = 0;
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         result += arr[i];
     }
     return result;
 }
 
-int subtract(int arr[], int size){
+int subtract(int arr[], int size)
+{
     int result = 0;
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         result -= arr[i];
     }
     return result;
 }
 
-int multiply(int arr[], int size){
+int multiply(int arr[], int size)
+{
     int result = 0;
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         result *= arr[i];
     }
     return result;
 }
 
-int division(int x , int y)
+int division(int x, int y)
 {
-    return x/y;
+    if (y == 0)
+    {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+    }
+    return x / y;
 }
 
 int sqrPower(int x)
 {
-    return x*x;
+    return x * x;
 }
 
 int modulas(int x, int y)
 {
- return x%y; 
+    if (y == 0)
+    {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+    };
+    return x % y;
 }
 
 int main()
 {
+    int size;
+    
+    cout << "Enter the number of values: ";
+    cin >> size;
 
+    int* arr = new int[size];
 }
